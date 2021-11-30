cd78575ae09d7ddcf828b66a8acd0f9f8845a3ed
felipedds <53063266+felipedds@users.noreply.github.com>
2021-11-29 22:37:07 -0300

    Add files via upload

--- iam-ec2-s3_resource-object.py
+++ iam-ec2-s3_resource-object.py
@@ -5,14 +5,19 @@ iam_console_resource = aws_management_console.resource(service_name='iam', regio
 ec2_console_resource = aws_management_console.resource(service_name='ec2', region_name='us-east-1')
 s3_console_resource = aws_management_console.resource(service_name='s3', region_name='us-east-1')
 
-# List all IAM users using resource object
-for user in iam_console_resource.users.all():
-    print(user.user_name)
+def list_iam_users(): # List all IAM users using resource object
+    for user in iam_console_resource.users.all():
+        print(user.user_name)
 
-# List all EC2 instances using resource object
-for instance in ec2_console_resource.instances.all():
-    print(instance.instanceid)
+def list_ec2_instances(): # List all EC2 instances using resource object
+    for instance in ec2_console_resource.instances.all():
+        print(instance.instanceid)
 
-# List all S3 buckets using resource object
-for bucket in s3_console_resource.buckets.all():
-    print(bucket.name)
+def list_s3_buckets(): # List all S3 buckets using resource object
+    for bucket in s3_console_resource.buckets.all():
+        print(bucket.name)
+
+if __name__ == '__main__':
+    print(list_iam_users())
+    print(list_ec2_instances())
+    print(list_s3_buckets())
